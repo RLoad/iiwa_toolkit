eb9522585b9f96796b32e10a1a46e35ee215f790
RLoad <wurui19930213@gmail.com>
2021-09-16 16:42:09 +0800

    recheck and add some comment, show where I change

--- src/force_control.cpp
+++ src/force_control.cpp
@@ -68,10 +68,11 @@ void ForceDS::update(const Eigen::Vector3d& vel, const Eigen::Vector3d& des_vel)
     {
         // compute damping
         updateDampingMatrix(des_vel);
-       //---- force control
-    //    control_output = eigVal0/250*des_vel;
+        //---- force control ------------------------------------------------------------
+        //control_output = eigVal0/250*des_vel;
         control_output = control_prev + eigVal0*des_vel;
         control_prev=control_output;
+        //------------------------------------------------------------------------------
         ROS_WARN_STREAM_THROTTLE(1, "forceDS!!!!!!!!!"<<des_vel(0)<<","<<des_vel(1)<<","<<des_vel(2)<<","<<control_output(0)<<","<<control_output(1)<<","<<control_output(2));
         // std::cerr << damping_eigval(0,0)<< " forceDS!!!!!!!!!"<<des_vel(0)<<","<<des_vel(1)<<","<<des_vel(2)<<","<<control_output(0)<<","<<control_output(1)<<","<<control_output(2)<<"\n";
     }
